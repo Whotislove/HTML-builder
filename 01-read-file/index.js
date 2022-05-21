91d8866bd75d928d287004320cc5b26055eb95b4
Whotislove <vlad.lobunets@mail.ru>
2022-05-21 16:41:38 +0300

    feat: update

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,3 +4,4 @@ const stream = fs.createReadStream(path.join(__dirname,'text.txt'),'utf-8');
 let data = ' ';
 stream.on('data', chunk => data += chunk);
 stream.on('end', () => console.log(data));
+stream.on('error',error=> console.log('Error', error));
