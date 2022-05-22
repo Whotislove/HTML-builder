c532a9e2a96d8983f3eb0a471c1c76292bcc0d22
Whotislove <vlad.lobunets@mail.ru>
2022-05-22 18:29:31 +0300

    feat: upgrade second tusk

---
+++ 02-write-file/index.js
@@ -0,0 +1,14 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'some.txt'));
+stdout.write('Привет, введите что нибудь\n');
+stdin.on('data', (data) => {
+  if(data.toString().trim()==='exit'){
+    process.exit();
+  } else{
+    output.write(data);
+  }
+}
+);
+process.on('exit', ()=> console.log('Приходите ещё'));
