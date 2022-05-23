47b657a87849a7503c53593a76eeebd03793787e
Whotislove <vlad.lobunets@mail.ru>
2022-05-23 16:42:51 +0300

    feat: add fourth tusk

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
-
+const fs = require('fs');
+const path = require('path');
+fs.mkdir(path.join(__dirname,'files-copy'),{recursive:true},err=>{
+  if (err) throw err;
+});
+function copyDir(){
+  fs.readdir(path.join(__dirname,'files'),(err, files)=>{
+    if(err){
+      console.error(err);
+      return;
+    } else {
+      for(const file of files){
+        const input = fs.createReadStream(path.join(__dirname,'files',file),'utf-8');
+        const output = fs.createWriteStream(path.join(__dirname,'files-copy',file));
+        input.on('data', chunk=> output.write(chunk));
+      }
+    }
+  }
+  );
+}
+copyDir();
